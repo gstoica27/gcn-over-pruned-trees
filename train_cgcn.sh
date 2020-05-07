d7b37a2b0fdfe4b5f3d510b25b2c2a51b4482a6a
gstoica27 <gstoica27@gmail.com>
2020-05-06 20:17:13 -0700

    Added cuda device for server use

--- train_cgcn.sh
+++ train_cgcn.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-SAVE_ID=$1
-python3.7 train.py --id $SAVE_ID --seed 0 --prune_k 1 --lr 0.3 --rnn_hidden 100 --num_epoch 200 --pooling max --mlp_layers 2 --pooling_l2 0.003
+DEVICE=$1
+SAVE_ID=$2
+CUDA_VISIBLE_DEVICES=$DEVICE python3.7 train.py --id $SAVE_ID --seed 0 --prune_k 1 --lr 0.3 --rnn_hidden 100 --num_epoch 200 --pooling max --mlp_layers 2 --pooling_l2 0.003
