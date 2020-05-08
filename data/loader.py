d72dce58d507a56b1eaed697b0616f20508c4471
gstoica27 <gstoica27@gmail.com>
2020-05-08 13:49:21 -0700

    Added corrected(?) mask filtering

--- data/loader.py
+++ data/loader.py
@@ -101,8 +101,8 @@ class DataLoader(object):
         ner = get_long_tensor(batch[2], batch_size)
         deprel = get_long_tensor(batch[3], batch_size)
         head = get_long_tensor(batch[4], batch_size)
-        subj_positions = get_long_tensor(batch[5], batch_size)
-        obj_positions = get_long_tensor(batch[6], batch_size)
+        subj_positions = get_long_tensor(batch[5], batch_size, fill_value=150)
+        obj_positions = get_long_tensor(batch[6], batch_size, fill_value=150)
         subj_type = get_long_tensor(batch[7], batch_size)
         obj_type = get_long_tensor(batch[8], batch_size)
 
@@ -123,10 +123,10 @@ def get_positions(start_idx, end_idx, length):
     return list(range(-start_idx, 0)) + [0]*(end_idx - start_idx + 1) + \
             list(range(1, length-end_idx))
 
-def get_long_tensor(tokens_list, batch_size):
+def get_long_tensor(tokens_list, batch_size, fill_value=constant.PAD_ID):
     """ Convert list of list of tokens to a padded LongTensor. """
     token_len = max(len(x) for x in tokens_list)
-    tokens = torch.LongTensor(batch_size, token_len).fill_(constant.PAD_ID)
+    tokens = torch.LongTensor(batch_size, token_len).fill_(fill_value)
     for i, s in enumerate(tokens_list):
         tokens[i, :len(s)] = torch.LongTensor(s)
     return tokens
