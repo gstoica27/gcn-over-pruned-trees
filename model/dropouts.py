248569db29984308a4d0a149538d35335fb54fc3
gstoica27 <gstoica27@gmail.com>
2020-05-14 20:43:24 -0700

    Added working variational dropout + removed DropConnections (pytorch bug)

--- model/dropouts.py
+++ model/dropouts.py
@@ -5,40 +5,20 @@ from torch.nn.utils.rnn import PackedSequence
 
 # Copied from: https://github.com/keitakurita/Better_LSTM_PyTorch/blob/master/better_lstm/model.py
 class VariationalDropout(nn.Module):
-    """
-    Applies the same dropout mask across the temporal dimension
-    See https://arxiv.org/abs/1512.05287 for more details.
-    Note that this is not applied to the recurrent activations in the LSTM like the above paper.
-    Instead, it is applied to the inputs and outputs of the recurrent layer.
-    """
-    def __init__(self, dropout, batch_first=False):
-        super(VariationalDropout).__init__()
+    def __init__(self, dropout=.5):
+        super(VariationalDropout, self).__init__()
         self.dropout = dropout
-        self.batch_first = batch_first
 
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
+    def forward(self, inputs):
         if not self.training or self.dropout <= 0.:
-            return x
+            return inputs
+        batch_size, num_tokens, encoding_dim = inputs.shape
+        mask = inputs.new_empty(batch_size, 1, encoding_dim, requires_grad=False).bernoulli_(1 - self.dropout)
+        if inputs.is_cuda:
+            mask = mask.cuda()
+        masked_inputs = inputs.masked_fill(mask == 0, 0) / (1 - self.dropout)
+        return masked_inputs
 
-        is_packed = isinstance(x, PackedSequence)
-        if is_packed:
-            x, batch_sizes = x
-            max_batch_size = int(batch_sizes[0])
-        else:
-            batch_sizes = None
-            max_batch_size = x.size(0)
-
-        # Drop same mask across entire sequence
-        if self.batch_first:
-            m = x.new_empty(max_batch_size, 1, x.size(2), requires_grad=False).bernoulli_(1 - self.dropout)
-        else:
-            m = x.new_empty(1, max_batch_size, x.size(2), requires_grad=False).bernoulli_(1 - self.dropout)
-        x = x.masked_fill(m == 0, 0) / (1 - self.dropout)
-
-        if is_packed:
-            return PackedSequence(x, batch_sizes)
-        else:
-            return x
 
 class EmbeddingDropout(nn.Module):
     def __init__(self, dropout=.5):
--- model/tree_lstm.py
+++ model/tree_lstm.py
@@ -4,6 +4,7 @@ import torch.nn.functional as F
 from torch.autograd import Variable
 import numpy as np
 from torchnlp.nn import *
+from model.dropouts import VariationalDropout
 
 
 # Copied from https://github.com/dasguptar/treelstm.pytorch/blob/master/treelstm/model.py
@@ -62,13 +63,10 @@ class BatchedChildSumTreeLSTM(nn.Module):
         self.h_dropout = h_dropout
         self.x_iouf = nn.Linear(self.in_dim, 4 * self.mem_dim)
         self.h_iou = nn.Linear(self.mem_dim, 3 * self.mem_dim)
-        self.h_iou_dropped = WeightDrop(self.h_iou,
-                                ['weight'],
-                                dropout=self.h_dropout)
         self.h_f = nn.Linear(self.mem_dim, self.mem_dim)
-        self.h_f_dropped = WeightDrop(self.h_f,
-                                              ['weight'],
-                                              dropout=self.h_dropout)
+
+        self.input_dropout = VariationalDropout(dropout=self.x_dropout)
+        self.output_dropout = VariationalDropout(dropout=self.x_dropout)
 
     def step(self, inputs, child_hidden, child_cell, child_mask):
         """
@@ -84,7 +82,7 @@ class BatchedChildSumTreeLSTM(nn.Module):
         num_children = child_hidden.shape[2]
         h_j = (child_hidden * child_mask).sum(2)          # [B,T1,H]
         x_iouf = self.x_iouf(inputs)                      # [B,T1,4H]
-        h_iou = self.h_iou_dropped(h_j)                           # [B,T1,3H]
+        h_iou = self.h_iou(h_j)                           # [B,T1,3H]
         x_i, x_o, x_u, x_f = torch.split(x_iouf, int(x_iouf.shape[-1] / 4), dim=2)
         h_i, h_o, h_u = torch.split(h_iou, int(h_iou.shape[-1] / 3), dim=2)
 
@@ -92,7 +90,7 @@ class BatchedChildSumTreeLSTM(nn.Module):
         o_j = torch.sigmoid(x_o + h_o)
         u_j = torch.tanh(x_u + h_u)
 
-        h_f = self.h_f_dropped(child_hidden)  # [B,T1,T2,H]
+        h_f = self.h_f(child_hidden)  # [B,T1,T2,H]
         f_jk = torch.sigmoid(x_f[:, :num_children, :].unsqueeze(1) + h_f)        # [B,T1,T2,H]
         # [B,T1,T2,H]x[B,T1,T2,H]x[B,T1,T2,1] -> [B,T1,H]
         c_j_rhs = (f_jk * child_cell * child_mask).sum(2)
@@ -101,13 +99,23 @@ class BatchedChildSumTreeLSTM(nn.Module):
         h = o_j * torch.tanh(c)
         return h, c
 
+    def hidden_dropout(self):
+        self.h_iou.weight = torch.nn.functional.dropout(self.h_iou.weight,
+                                                             p=self.h_dropout,
+                                                             training=self.training).contiguous()
+        self.h_f.weight.data = torch.nn.functional.dropout(self.h_f.weight.data,
+                                                             p=self.h_dropout,
+                                                             training=self.training).contiguous()
+
     def forward(self, token_encodings, trees, child_mask, max_depth):
+        # self.hidden_dropout()
         batch_size, token_size, hidden_dim = token_encodings.shape
         # hidden: [B,T1+2,H] cell: [B,T1+2,H]
         hidden_state, cell_state = self.init_zero_state(
             batch_size=batch_size, token_size=token_size,
             hidden_dim=self.mem_dim, use_cuda=self.on_cuda
         )
+        dropped_encodings = self.input_dropout(token_encodings)
         for level in range(max_depth):
             # Flatten hidden/cell state in order perform lookup
             # [B,T1,H] -> [BxT1,H]
@@ -119,7 +127,7 @@ class BatchedChildSumTreeLSTM(nn.Module):
             child_cell_states = F.embedding(trees.type(torch.long), flat_cell_states, 0, 2, False, False)
 
             new_hidden_states, new_cell_states = self.step(
-                token_encodings,  # [:,:max_bottom_offset,:],
+                dropped_encodings,  # [:,:max_bottom_offset,:],
                 child_hidden_states,
                 child_cell_states,
                 child_mask
@@ -146,6 +154,8 @@ class BatchedChildSumTreeLSTM(nn.Module):
             else:
                 hidden_state = new_hidden_states
                 cell_state = new_cell_states
+
+        hidden_state = self.output_dropout(hidden_state)
         return hidden_state
 
     def init_zero_state(self, batch_size, token_size, hidden_dim, use_cuda=False):
