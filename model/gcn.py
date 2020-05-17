f1e1fc048fe1909ec997d21d21126aae863d630e
gstoica27 <gstoica27@gmail.com>
2020-05-17 03:08:35 -0700

    slight fix

--- model/gcn.py
+++ model/gcn.py
@@ -84,6 +84,7 @@ class GCNRelationModel(nn.Module):
 
     def forward(self, inputs):
         words, masks, pos, ner, deprel, head, subj_pos, obj_pos, subj_type, obj_type = inputs # unpack
+        print('words: {} | masks: {} | pos: {} | ner: {}'.format(words.shape, masks.shape, pos.shape, ner.shape))
         l = (masks.data.cpu().numpy() == 0).astype(np.int64).sum(1)
         maxlen = max(l)
 
