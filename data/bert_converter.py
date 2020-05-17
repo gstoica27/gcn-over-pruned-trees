8da1843bba80db239718febdd666ccc680903f53
gstoica27 <gstoica27@gmail.com>
2020-05-16 21:28:19 -0700

    adding bert converter stuff

--- data/bert_converter.py
+++ data/bert_converter.py
@@ -14,7 +14,7 @@ def load_datasets(file_dir, file_names):
         data += partition_data
     return data
 
-def replace_tokens(data):
+def replace_tokens(tokens):
     mappings = {
             '-LRB-': '(',
             '-RRB-': ')',
@@ -23,9 +23,39 @@ def replace_tokens(data):
             '-LCB-': '{',
             '-RCB-': '}'
         }
+    return [token if token not in mappings else mappings[token] for token in tokens]
+
+def extract_ids_and_sentences(data):
+    id2sentences = {}
     for d in data:
-        d['token'] = [token if token not in mappings else mappings[token] for token in d['token']]
-    return data
+        tokens = list(d['token'])
+        tokens = replace_tokens(tokens)
+        # anonymize tokens
+        ss, se = d['subj_start'], d['subj_end']
+        os, oe = d['obj_start'], d['obj_end']
+        tokens[ss:se + 1] = ['SUBJ-' + d['subj_type']] * (se - ss + 1)
+        tokens[os:oe + 1] = ['OBJ-' + d['obj_type']] * (oe - os + 1)
+        token_id = d['id']
+        id2sentences[token_id] = tokens
+
+    id2sentences_ascending = sorted(id2sentences.items(), key=lambda kv: len(kv[1]))
+    # ids, sentences = zip(*id2sentences_ascending)
+    return id2sentences_ascending
+
+def batch_data(data, batch_size=100):
+    batched_data = []
+    for batch_start in range(0, len(data), batch_size):
+        batch = data[batch_start: batch_start + batch_size]
+        batched_data.append(batch)
+    return batched_data
+
+def extract_embeddings(batches, save_dir):
+    bert_client = BertClient()
+    id2sentence_embeddings = {}
+    for batch in batches:
+        ids, sentences = zip(*batch)
+
+
 
 def save_data(data, save_path):
     with open(save_path, 'wb') as handle:
