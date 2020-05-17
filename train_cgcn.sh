fd7fbaf85b46964c6536106a9944b68a91b6fd43
George I Stoica <gis@gpu2.learning.cs.cmu.edu>
2020-05-16 21:50:34 -0400

    exp changes

--- train_cgcn.sh
+++ train_cgcn.sh
@@ -2,4 +2,4 @@
 
 DEVICE_ID=$1
 SAVE_ID=$2
-CUDA_VISIBLE_DEVICES=$DEVICE_ID python3.7 train.py --id $SAVE_ID --seed 0 --prune_k 1 --lr .3 --rnn_hidden 200 --num_epoch 200 --pooling max --mlp_layers 2 --pooling_l2 0.003 --batch_size 50 --adj_type concat_deprel --log_step 20 --deprel_emb_dim 125 --deprel_dropout .1
+CUDA_VISIBLE_DEVICES=$DEVICE_ID python3.7 train.py --id $SAVE_ID --seed 0 --prune_k 1 --lr .3 --rnn_hidden 200 --num_epoch 200 --pooling max --mlp_layers 2 --pooling_l2 0.003 --batch_size 25 --log_step 40 --num_tree_lstms 1 --tree_x_dropout .0 --input_dropout .1 --deprel_emb_dim 0 --node_pooling True
