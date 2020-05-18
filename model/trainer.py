1b88e1578c52621d2c49f72ff80e44606fe6b3c5
gstoica27 <gstoica27@gmail.com>
2020-05-18 14:02:24 -0700

    Fixed slight bug

--- model/trainer.py
+++ model/trainer.py
@@ -119,7 +119,7 @@ class GCNTrainer(Trainer):
 
     def predict(self, batch, unsort=True):
         inputs, labels, tokens, head, subj_pos, obj_pos, lens = unpack_batch(batch, self.opt['cuda'])
-        orig_idx = batch[11]
+        orig_idx = batch[-1]
         # forward
         self.model.eval()
         logits, pooling_output, component_encs = self.model(inputs)
