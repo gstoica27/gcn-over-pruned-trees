ec58d1f4c29e50d13bada1a861e222cf0984c821
gstoica27 <gstoica27@gmail.com>
2020-05-18 16:51:37 -0700

    Fixed self induced num component bug

--- model/trainer.py
+++ model/trainer.py
@@ -47,25 +47,20 @@ class Trainer(object):
 
 
 def unpack_batch(batch, cuda):
+    if cuda:
+        inputs = [Variable(b.cuda()) for b in batch[:-2]]
+        labels = Variable(batch[-2].cuda())
+    else:
+        inputs = [Variable(b) for b in batch[:-2]]
+        labels = Variable(batch[-2])
+
     if len(batch) >= 10:
-        if cuda:
-            inputs = [Variable(b.cuda()) for b in batch[:10]]
-            labels = Variable(batch[10].cuda())
-        else:
-            inputs = [Variable(b) for b in batch[:10]]
-            labels = Variable(batch[10])
         tokens = batch[0]
         head = batch[5]
         subj_pos = batch[6]
         obj_pos = batch[7]
         lens = batch[1].eq(0).long().sum(1).squeeze()
     else:
-        if cuda:
-            inputs = [Variable(b.cuda()) for b in batch[:-2]]
-            labels = Variable(batch[-2].cuda())
-        else:
-            inputs = [Variable(b) for b in batch[:-2]]
-            labels = Variable(batch[-2])
         tokens = batch[0]
         head = batch[4]
         subj_pos = batch[5]
